731a1bdc34400c0c975568c954d3e18f2bf785a0
Ahmed Gamal <ahmedmgh67@gmail.com>
2019-03-15 11:41:15 +0200

    API Changes
    @abdelrahim123  Go And use The Model Provided

--- js/app.js
+++ js/app.js
@@ -1,7 +1,53 @@
- import Fetch from 'fetch';
+ //import Fetch, { fetchUrl } from 'fetch';
  
- document.getElementById("userName").innerHTML= a();
+ document.getElementById("userName").innerHTML= getUserName();
+ var username = document.getElementById("login-username");
+ var password = document.getElementById("login-password");
+// var loginBu = document.getElementbyId('btn-login');
 
-function getUserName(){
+/*loginBu.addEventListener('click', function a (){
+  alert('login buttton clicked event listener');
+  login();
+});*/
 
+/*function login (){
+  //alert('login buttton clicked function');
+  var user = username.value;
+  var pass = password.value;
+
+  fetch('https://stacknet-api.herokuapp.com/api/tpa')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    console.log(data);
+    alert("login began")
+    // Work with JSON data here
+    if (data.username == user){
+      window.location("./dashboard.html")
+    } else {
+      alert("Login Failed");
+    }
+  })
+  .catch(err => {
+    // Do something for an error here
+  })
+  return"Ahmed Gamal";
+  
+} */
+function getUserName () {
+  fetch('https://stacknet-api.herokuapp.com/api/sps')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    // Work with JSON data here
+    console.log(data);
+    return data.name;
+  })
+  .catch(err => {
+    // Do something for an error here
+    return "error"
+  })
+  
 }
